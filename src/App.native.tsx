4df96bc40988fcefd0bef498316ce21026563d06
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2025-04-23 12:47:04 +0300

    fix sentry

--- src/App.native.tsx
+++ src/App.native.tsx
@@ -1,10 +1,10 @@
 import 'react-native-url-polyfill/auto'
-import '#/lib/sentry'; // must be near top
+// import '#/lib/sentry'; // must be near top
 import '#/view/icons'
 
-import React, { useEffect, useState } from 'react'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { RootSiblingParent } from 'react-native-root-siblings'
+import React, {useEffect, useState} from 'react'
+import {GestureHandlerRootView} from 'react-native-gesture-handler'
+import {RootSiblingParent} from 'react-native-root-siblings'
 import {
   initialWindowMetrics,
   SafeAreaProvider,
@@ -12,54 +12,54 @@ import {
 import * as ScreenOrientation from 'expo-screen-orientation'
 import * as SplashScreen from 'expo-splash-screen'
 import * as SystemUI from 'expo-system-ui'
-import { msg } from '@lingui/macro'
-import { useLingui } from '@lingui/react'
+import {msg} from '@lingui/macro'
+import {useLingui} from '@lingui/react'
 
-import { KeyboardControllerProvider } from '#/lib/hooks/useEnableKeyboardController'
-import { QueryProvider } from '#/lib/react-query'
-import { Provider as StatsigProvider, tryFetchGates } from '#/lib/statsig/statsig'
-import { s } from '#/lib/styles'
-import { ThemeProvider } from '#/lib/ThemeContext'
+import {KeyboardControllerProvider} from '#/lib/hooks/useEnableKeyboardController'
+import {QueryProvider} from '#/lib/react-query'
+import {Provider as StatsigProvider, tryFetchGates} from '#/lib/statsig/statsig'
+import {s} from '#/lib/styles'
+import {ThemeProvider} from '#/lib/ThemeContext'
 import I18nProvider from '#/locale/i18nProvider'
-import { logger } from '#/logger'
-import { isAndroid, isIOS } from '#/platform/detection'
-import { Provider as A11yProvider } from '#/state/a11y'
-import { Provider as MutedThreadsProvider } from '#/state/cache/thread-mutes'
-import { Provider as DialogStateProvider } from '#/state/dialogs'
-import { listenSessionDropped } from '#/state/events'
+import {logger} from '#/logger'
+import {isAndroid, isIOS} from '#/platform/detection'
+import {Provider as A11yProvider} from '#/state/a11y'
+import {Provider as MutedThreadsProvider} from '#/state/cache/thread-mutes'
+import {Provider as DialogStateProvider} from '#/state/dialogs'
+import {listenSessionDropped} from '#/state/events'
 import {
   beginResolveGeolocation,
   ensureGeolocationResolved,
   Provider as GeolocationProvider,
 } from '#/state/geolocation'
-import { Provider as HomeBadgeProvider } from '#/state/home-badge'
-import { Provider as InvitesStateProvider } from '#/state/invites'
-import { Provider as LightboxStateProvider } from '#/state/lightbox'
-import { MessagesProvider } from '#/state/messages'
-import { Provider as ModalStateProvider } from '#/state/modals'
-import { init as initPersistedState } from '#/state/persisted'
-import { Provider as PrefsStateProvider } from '#/state/preferences'
-import { Provider as LabelDefsProvider } from '#/state/preferences/label-defs'
-import { Provider as ModerationOptsProvider } from '#/state/preferences/moderation-opts'
-import { Provider as UnreadNotifsProvider } from '#/state/queries/notifications/unread'
+import {Provider as HomeBadgeProvider} from '#/state/home-badge'
+import {Provider as InvitesStateProvider} from '#/state/invites'
+import {Provider as LightboxStateProvider} from '#/state/lightbox'
+import {MessagesProvider} from '#/state/messages'
+import {Provider as ModalStateProvider} from '#/state/modals'
+import {init as initPersistedState} from '#/state/persisted'
+import {Provider as PrefsStateProvider} from '#/state/preferences'
+import {Provider as LabelDefsProvider} from '#/state/preferences/label-defs'
+import {Provider as ModerationOptsProvider} from '#/state/preferences/moderation-opts'
+import {Provider as UnreadNotifsProvider} from '#/state/queries/notifications/unread'
 import {
   Provider as SessionProvider,
-  SessionAccount,
+  type SessionAccount,
   useSession,
   useSessionApi,
 } from '#/state/session'
-import { readLastActiveAccount } from '#/state/session/util'
-import { Provider as ShellStateProvider } from '#/state/shell'
-import { Provider as ComposerProvider } from '#/state/shell/composer'
-import { Provider as LightStatusBarProvider } from '#/state/shell/light-status-bar'
-import { Provider as LoggedOutViewProvider } from '#/state/shell/logged-out'
-import { Provider as ProgressGuideProvider } from '#/state/shell/progress-guide'
-import { Provider as SelectedFeedProvider } from '#/state/shell/selected-feed'
-import { Provider as StarterPackProvider } from '#/state/shell/starter-pack'
-import { Provider as HiddenRepliesProvider } from '#/state/threadgate-hidden-replies'
-import { Provider as TrendingConfigProvider } from '#/state/trending-config'
-import { TestCtrls } from '#/view/com/testing/TestCtrls'
-import { Provider as VideoVolumeProvider } from '#/view/com/util/post-embeds/VideoVolumeContext'
+import {readLastActiveAccount} from '#/state/session/util'
+import {Provider as ShellStateProvider} from '#/state/shell'
+import {Provider as ComposerProvider} from '#/state/shell/composer'
+import {Provider as LightStatusBarProvider} from '#/state/shell/light-status-bar'
+import {Provider as LoggedOutViewProvider} from '#/state/shell/logged-out'
+import {Provider as ProgressGuideProvider} from '#/state/shell/progress-guide'
+import {Provider as SelectedFeedProvider} from '#/state/shell/selected-feed'
+import {Provider as StarterPackProvider} from '#/state/shell/starter-pack'
+import {Provider as HiddenRepliesProvider} from '#/state/threadgate-hidden-replies'
+import {Provider as TrendingConfigProvider} from '#/state/trending-config'
+import {TestCtrls} from '#/view/com/testing/TestCtrls'
+import {Provider as VideoVolumeProvider} from '#/view/com/util/post-embeds/VideoVolumeContext'
 import * as Toast from '#/view/com/util/Toast'
 import {Shell} from '#/view/shell'
 import {ThemeProvider as Alf} from '#/alf'
@@ -238,4 +238,4 @@ function App() {
   )
 }
 
-export default Sentry.wrap(App)
+export default App
